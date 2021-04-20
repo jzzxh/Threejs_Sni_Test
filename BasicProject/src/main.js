b0da47189b22a09c07fa9f91606badef66f683cc
JasonHuang <jzzxhciu@gmail.com>
2021-04-20 21:17:29 +0800

    add horse glb file test, model move on the path but rotation error

--- BasicProject/src/main.js
+++ BasicProject/src/main.js
@@ -3,16 +3,17 @@ var linePoints = [];
 var pLinePoints = [];
 var lastChange;
 
-
 // Follow Path init
 var up = new THREE.Vector3(1, 0, 0);
 var axis = new THREE.Vector3();
 var marker;
 var pt, radians, axis, tangent, pathPxn;
+
+// GLB file
+var mixer, clock, model;
+
 // the getPoint starting variable - !important - You get me ;)
-var t = 0; 
-
-
+var t = 0.91;
 
 function getCube() {
   // cube mats and cube
@@ -27,14 +28,10 @@ function getCube() {
     );
   }
 
-  var cube = new THREE.Mesh(
-    new THREE.BoxGeometry(2, 2, 2),
-    mats
-  );
+  var cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), mats);
 
   return cube;
 }
-
 
 function init() {
   const container = document.querySelector("#scene-container");
@@ -49,7 +46,7 @@ function init() {
   const far = 100;
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
-  camera.position.set(0, 5, 20);
+  camera.position.set(0, 8, 30);
 
   // controls
   controls = new THREE.OrbitControls(camera, container);
@@ -57,7 +54,7 @@ function init() {
   controls.minDistance = 10;
   controls.maxDistance = 50;
 
-    ////////////////////////////////////////
+  ////////////////////////////////////////
   //      Create the cube               //
   ////////////////////////////////////////
 
@@ -65,49 +62,49 @@ function init() {
   marker.position.set(0, 0, 0);
   scene.add(marker);
 
-
-  // var mats = [];
-  // for (var i = 0; i < 6; i++) {
-  //   mats.push(
-  //     new THREE.MeshBasicMaterial({
-  //       color: Math.random() * 0xffffff,
-  //       opacity: 0.6,
-  //       transparent: true,
-  //     })
-  //   );
-  // }
-
-  // const geometry = new THREE.BoxBufferGeometry(2, 2, 2);
-
-  // //   const material = new THREE.MeshBasicMaterial(mats);
-  // // const facematerial = new THREE.MeshBasicMaterial(mats); //dec
-
-  // const cube = new THREE.Mesh(geometry, mats);
-
-  // scene.add(cube);
-
-  // Create Line
-  //   const lineMaterial = new THREE.LineBasicMaterial({ color: 'red' });
-
-  //   const points = [];
-  //   points.push(new THREE.Vector3(-10, 0, 0));
-  //   points.push(new THREE.Vector3(0, 10, 0));
-  //   points.push(new THREE.Vector3(10, 0, 0));
-
-  //   const linGeometry = new THREE.BufferGeometry().setFromPoints(points);
-
-  //   const line = new THREE.Line(linGeometry, lineMaterial);
-
-  //   scene.add(line);
-
   // Load SVG file
-  loadSVG("./runway2.svg");
+  loadSVG("./runway2.svg"); // runway2
 
   //   LineCatmullroom();
 
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
-  directionalLight.position.set(0.75, 0.75, 1.0).normalize();
+  ///////////////////////
+  //  Load GLTF FILE   //
+  ///////////////////////
+  const loader = new THREE.GLTFLoader();
+  clock = new THREE.Clock();
+  // Load a glTF resource
+  loader.load(
+    // resource URL
+    "model/Horse.glb",
+    // called when the resource is loaded
+    function (gltf) {
+      model = gltf.scene;
+      model.scale.set(0.02, 0.02, 0.02); // scale here
+      
+      mixer = new THREE.AnimationMixer(model);
+      var action = mixer.clipAction(gltf.animations[0]);
+      action.play();
+
+      scene.add(model);
+
+    },
+    // called while loading is progressing
+    function (xhr) {
+      console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+    },
+    // called when loading has errors
+    function (error) {
+      console.log("An error happened");
+    }
+  );
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+  directionalLight.position.set(0.75, 0.75, -1.0).normalize();
   scene.add(directionalLight);
+
+  const directionalLight2 = new THREE.DirectionalLight(0xffffff, 2);
+  directionalLight2.position.set(0.75, 0.75, 1.0).normalize();
+  scene.add(directionalLight2);
 
   scene.add(new THREE.AxesHelper(10));
 
@@ -128,15 +125,8 @@ function init() {
 function loadSVG(url) {
   const loader = new THREE.SVGLoader();
 
-  loader.load(url, function(data) {
+  loader.load(url, function (data) {
     const paths = data.paths;
-    const group = new THREE.Group();
-    group.scale.multiplyScalar(0.005);
-    group.position.x = 0;
-    group.position.y = 0;
-    group.scale.y *= -1;
-
-    // console.log(paths);
 
     for (let i = 0; i < paths.length; i++) {
       const path = paths[i];
@@ -156,34 +146,21 @@ function loadSVG(url) {
             subPath.getPoints()[k].y * 0.01 - 5,
           ]);
         }
-
-        // const geometry = THREE.SVGLoader.pointsToStroke(
-        //   subPath.getPoints(),
-        //   path.userData.style
-        // );
-
-        // if (geometry) {
-        //   const mesh = new THREE.Mesh(geometry, material);
-        //   group.add(mesh);
-        // }
       }
     }
 
-    // pLinePoints = _.cloneDeep(linePoints);
-    // console.log(linePoints.length);
-
-    scene.add(group);
   });
-
-  // console.log("plinpoints: " + pLinePoints.length);
-  // console.log("Test ponts: " + testPoints2.length);
 }
 
 function LineCatmullroom() {
   //Hard coded array of points
-
-  var points = [[0, 2], [2, 10], [-1, 15], [-3, 20], [0, 25]];
-
+  // var points = [
+  //   [0, 2],
+  //   [2, 10],
+  //   [-1, 15],
+  //   [-3, 20],
+  //   [0, 25],
+  // ];
   //Convert the array of points into vertices
   //   for (var i = 0; i < points.length; i++) {
   //     var x = points[i][0];
@@ -191,14 +168,12 @@ function LineCatmullroom() {
   //     var z = points[i][1];
   //     points[i] = new THREE.Vector3(x, y, z);
   //   }
-
   // for (var i = 0; i < linePoints.length; i++) {
   //     var x = linePoints[i][0];
   //     var y = 0;
   //     var z = linePoints[i][1] ;
   //     pLinePoints.push(new THREE.Vector3(x, y, z));
   //   }
-
   //Create a path from the points
   //   var path = new THREE.CatmullRomCurve3(points);
   //   var geometry = new THREE.TubeGeometry(path, 64, 2, 8, false);
@@ -208,21 +183,16 @@ function LineCatmullroom() {
   //   var tube = new THREE.Mesh(geometry, material);
   //Add tube into the scene
   //   scene.add(tube);
-
   //   const pathPxn = new THREE.CatmullRomCurve3(linePoints);
   //   const pointsPTS = pathPxn.getPoints(50);
   //   const geometry = new THREE.BufferGeometry().setFromPoints(pointsPTS);
-
   //   const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
-
   //   // Create the final object to add to the scene
   //   const curveObject = new THREE.Line(geometry, material);
-
   //   scene.add(curveObject);
 }
 
 function lineDraw() {
-   
   console.log(linePoints[0][0]); //Trick delay frame pass data to plinePoints.
 
   for (var i = 0; i < linePoints.length; i++) {
@@ -244,13 +214,17 @@ function render() {
 
     if (pLinePoints.length > 0) {
       //Create a path from the points
-       pathPxn = new THREE.CatmullRomCurve3(pLinePoints);
-      const pointsPTS = pathPxn.getPoints(100);
+      pathPxn = new THREE.CatmullRomCurve3(pLinePoints);
+      pathPxn.curveType = "catmullrom";
+      pathPxn.closed = true;
+      pointsPTS = pathPxn.getPoints(205);
       const geometry = new THREE.BufferGeometry().setFromPoints(pointsPTS);
       const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
 
       // Create the final object to add to the scene
       const curveObject = new THREE.Line(geometry, material);
+
+      console.log(pathPxn);
 
       scene.add(curveObject);
     }
@@ -264,23 +238,29 @@ function render() {
 
     // set the marker position
     marker.position.set(pt.x, pt.y, pt.z);
-  
+    model.position.set(pt.x,pt.y,pt.z);
     // get the tangent to the curve
     tangent = pathPxn.getTangent(t).normalize();
-  
+
     // calculate the axis to rotate around
     axis.crossVectors(up, tangent).normalize();
-  
+
     // calcluate the angle between the up vector and the tangent
-    // radians = Math.acos( up.dot( tangent ) );
     radians = Math.acos(up.dot(tangent));
-  
+
     // set the quaternion
     marker.quaternion.setFromAxisAngle(axis, radians);
-  
-    t = t >= 1 ? 0 : (t += 0.001);
-
+    model.quaternion.setFromAxisAngle(axis, radians);
+    // model.rotation.y = -1.5;
+    //model.rotation.y = -1.5;
+     t = t >= 1 ? 0 : (t += 0.001);
+    // t = t <= -0.09 ? -0.09 : (t -= 0.001);
   }
+
+  // GLB Animation
+  var delta = clock.getDelta();
+
+  if (mixer) mixer.update(delta);
 
   renderer.render(scene, camera);
 }
